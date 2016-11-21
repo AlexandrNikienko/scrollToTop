eb7cfef0b4dd14f50379f56595dc421fff17cfb2
alexandr.nikienko <alexandr.nikienko@gmail.com>
2016-11-21 22:00:58 +0200

    js optimization

--- scrollToTop_by_ALIENPLANET.js
+++ scrollToTop_by_ALIENPLANET.js
@@ -8,18 +8,19 @@ $(function () {
 
 var scrollToTop = {
     init: function() {
+        var that = this;
         $(document).scroll(function () {
             if ($(this).scrollTop() > 100) {
                 var scrollVal = $(this).scrollTop();
                 if (!$(".scroll-to-top-btn ").length) {
-                    $("body").append("<div class='scroll-to-top-btn'></div>");
+                    $("body").append("<div class='scroll-to-top-btn'/>");
                     $(".scroll-to-top-btn")
                         .fadeIn("fast")
                         .on("click", function () {
                             $(this).animate({
                                 "bottom": "100%",
                                 "opacity": "0"
-                            }, 500, this.ifJQueryUILoaded("easeInOutQuart"));
+                            }, 500, that.ifJQueryUILoaded("easeInOutQuart"));
                             $("body").animate({
                                 scrollTop: 0
                             }, 500);
